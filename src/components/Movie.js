1b1713e529a79bf9a5a42b54c3be831239bf4322
athiragithubworld <athirahrms@gmail.com>
2023-05-19 21:05:37 +0530

    Movies js ( Storing and deleting from backend )

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
+  async function deleteMovieHandler(MovieId) {
+    const response = await fetch(
+      `https://react-http-1b7b8-default-rtdb.firebaseio.com/movies/${MovieId}.json`,
+      {
+        method: "DELETE",
+        body: JSON.stringify(MovieId),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+  }
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button
+        style={{ background: "rgb(196, 68, 221)" }}
+        onClick={() => deleteMovieHandler(props.id)}
+      >
+        Delete
+      </button>
     </li>
   );
 };
